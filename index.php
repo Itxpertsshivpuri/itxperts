17f96352bd389cd52c94f69867ee5dbd6fcf6b2f
VIkram Singh Rawat <82854405+mritxperts@users.noreply.github.com>
2022-01-16 11:55:16 +0530

    Update index.php

--- index.php
+++ index.php
@@ -82,7 +82,7 @@
               <img src="assets/images/logo.png" height="80px" width="80px" alt="Itxperts Logo">
             </a>
             
-            <a class="logo"><span class="fa">&#xf232;</span></a>
+            <i class="logo"><span class="fa">&#xf232;</span></i>
             
             <!-- ***** Logo End ***** -->
             <!-- ***** Menu Start ***** -->
